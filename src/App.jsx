f87cc0614b465f1415fe6bb8bc9638e89b491524
Deepak Kumar Pandey <114860902+Deepak7206237949@users.noreply.github.com>
2024-10-01 23:50:02 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Navbar from './Navbar';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+import Appointment from './Appointment';
+import Home from './Home';
+import SignUp from './SignUp';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/appointment" element={<Appointment />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
